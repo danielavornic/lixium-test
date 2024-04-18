1e3100a6f7902a35eb3c5397715d0d4f58f56bef
Daniela Vornic <daniela.vornic@gmail.com>
2024-04-18 09:32:30 +0300

    Create 404.tsx

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "@/components/layout";
+
+const ErrorPage = () => {
+  return (
+    <Layout title="Error">
+      <h1 className="mb-2 text-2xl font-bold">404 Error</h1>
+      <p>Page not found.</p>
+    </Layout>
+  );
+};
+
+export default ErrorPage;
