d89454d55cac386442119273e89bca3fc655ce3e
Daniela Vornic <daniela.vornic@gmail.com>
2024-04-18 09:32:28 +0300

    Add NextProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import NextProgress from "next-progress";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <NextProgress color="#29D" options={{ showSpinner: false }} />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
